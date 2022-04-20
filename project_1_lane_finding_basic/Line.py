52df0a6485d049dda16d40fad4b5c50a93dd6ba0
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-04-21 00:03:21 +0530

    Update Line.py

--- project_1_lane_finding_basic/Line.py
+++ project_1_lane_finding_basic/Line.py
@@ -4,7 +4,7 @@ import cv2
 
 class Line:
     """ 
-    A Line is defined from two points (x1, y1) and (x2, y2) as follows:
+    A Line is defined from two points (x1, y1) and (x2, y2) as follows: 
     y - y1 = (y2 - y1) / (x2 - x1) * (x - x1)
     Each line has its own slope and intercept (bias).
     """ 
