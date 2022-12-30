2e8ab99943abfe32e098d52c160669ddf2540a58
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-12-30 06:10:35 +0530

    Update CholmodSupport.h

--- CholmodSupport/CholmodSupport.h
+++ CholmodSupport/CholmodSupport.h
@@ -27,7 +27,7 @@ void cholmod_configure_matrix(CholmodType& mat)
     mat.xtype = CHOLMOD_REAL;
     mat.dtype = CHOLMOD_DOUBLE;
   }
-  else if (internal::is_same<Scalar,std::complex<float> >::value)
+  else if (internal::is_same<Scalar,std::complex<float> >::value) 
   { 
     mat.xtype = CHOLMOD_COMPLEX;
     mat.dtype = CHOLMOD_SINGLE; 
